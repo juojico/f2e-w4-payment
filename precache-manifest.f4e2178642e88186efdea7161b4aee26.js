09decc3653f2cdc3001830570a9092368437190c
jico <u4m062@gmail.com>
2019-08-07 10:33:25 +0800

    Updates

---
+++ precache-manifest.f4e2178642e88186efdea7161b4aee26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a248d0281bb918a7f0898ad9be59de15",
+    "url": "/f2e-w4-payment/index.html"
+  },
+  {
+    "revision": "27b51bab6a8f8d4bb410",
+    "url": "/f2e-w4-payment/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "856281628acf98f4f10c",
+    "url": "/f2e-w4-payment/static/js/2.93bfb7ad.chunk.js"
+  },
+  {
+    "revision": "27b51bab6a8f8d4bb410",
+    "url": "/f2e-w4-payment/static/js/main.3147851a.chunk.js"
+  },
+  {
+    "revision": "13602b045ad68b3f537b",
+    "url": "/f2e-w4-payment/static/js/runtime~main.6e85bf1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/f2e-w4-payment/static/media/logo.5d5d9eef.svg"
+  }
+]);
